99e9a86b3f2c2cf6e64cbeb8487a0838b70d8c57
kwakuTM <kwakutm@yahoo.com>
2018-05-28 10:32:43 +0200

    Ransac foundation

---
+++ Pransac.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <string>
+#include <omp.h>
+#include <algorithm>
+#include <vector>
+#include <memory>
+
+#include "DimModel.hpp"
+
+
+namespace PRANSAC
+{ 
+   // T - AbstractPlane
+	template <class T, int planeParams>
+    class Ransac
+    {};
+}
