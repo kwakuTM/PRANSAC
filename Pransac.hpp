f5c276362789f8691493685854a85d070e2eeed0
kwakuTM <kwakutm@yahoo.com>
2018-05-29 09:37:24 +0200

    adding def

--- Pransac.hpp
+++ Pransac.hpp
@@ -15,7 +15,12 @@
 namespace PRANSAC
 { 
    // T - AbstractPlane
-	template <class T, int planeParams>
+	template <class P, int planeParams>
     class Ransac
-    {};
+    {
+    private:
+    	std::vector<std::shared_ptr<P>> allData; //All the data
+    	std::vector<std::shared_ptr<P>> allSampledModels; //vector of sampled models
+    	std::vector<P> m_BestModel;
+    };
 }
