765621fa0851f790e9175daaec2ca18e0885a771
kwakuTM <kwakutm@yahoo.com>
2018-05-30 13:00:37 +0200

    class ransac extended

--- Pransac.hpp
+++ Pransac.hpp
@@ -21,6 +21,15 @@ namespace PRANSAC
     private:
     	std::vector<std::shared_ptr<P>> allData; //All the data
     	std::vector<std::shared_ptr<P>> allSampledModels; //vector of sampled models
-    	std::vector<P> m_BestModel;
-    };
+    	std::vector<P> m_BestModel; // the best model
+    	std::vector<std::vector<std::shared_ptr<DimSpace>>> m_BestInliers;
+    	std::vector<std::shared_ptr<DimSpace>> sampledPoints;
+
+    	int maxIterations; // Number of iterations before terminations
+    	NPfloat threshold; // the threshold buffer
+    	NPfloat bestModelScore;
+    	int bestModelIdx; 
+
+    	std::vector<std::mt19937>m_RandEnginers;  // Mersenne twister high quality RNG that support *OpenMP* multi-threading
+    }; 
 }
