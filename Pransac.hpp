be777771625568bda4e50d6755f4f59e968eca5e
kwakuTM <kwakutm@yahoo.com>
2018-06-02 08:58:08 +0200

    continuation

--- Pransac.hpp
+++ Pransac.hpp
@@ -21,8 +21,8 @@ namespace PRANSAC
     private:
     	std::vector<std::shared_ptr<P>> allData; //All the data
     	std::vector<std::shared_ptr<P>> allSampledModels; //vector of sampled models
-    	std::vector<P> m_BestModel; // the best model
-    	std::vector<std::vector<std::shared_ptr<DimSpace>>> m_BestInliers;
+    	std::vector<P> bestModel; // the best model
+    	std::vector<std::vector<std::shared_ptr<DimSpace>>> bestInliers;
     	std::vector<std::shared_ptr<DimSpace>> sampledPoints;
 
     	int maxIterations; // Number of iterations before terminations
@@ -30,11 +30,40 @@ namespace PRANSAC
     	NPfloat bestModelScore;
     	int bestModelIdx; 
 
-    	std::vector<std::mt19937>m_RandEnginers;  // Mersenne twister high quality RNG that support *OpenMP* multi-threading
+    	std::vector<std::mt19937>RandEnginers;  // Mersenne twister high quality RNG that support *OpenMP* multi-threading
 
     public:
         RANSAC(void)
-	{
-             int threads = std::max(1, omp_get_max_threads());
-     	}; 
-}
+	    {
+             int num_threads = std::max(1, omp_get_max_threads());
+             std::cout << "[INFO]: Maximum number of usuable threads: " << num_threads << std::endl;
+             for(int i = 0; i < num_threads; ++i)
+             {
+                std::random_device seedDevice;
+                RandEnginers.push_back(std::mt19937(seedDevice()));
+             }
+
+             Reset();
+     	};
+
+        virtual ~RANSAC(void){}; 
+
+        void Initialize(NPfloat threshold, int maxIterations = 1000)
+        {
+            threshold = threshold;
+            maxIterations = maxIterations;
+        };
+
+        void Reset(void) 
+        {
+            //Clear sampled data for next iteration
+            allData.clear();
+            allSampledModels.clear();
+
+            bestModelIdx = -1;
+            bestModelScore = 0;
+        };
+
+        const std::vector<std::shared_ptr<DimSpace>>& GetRemainingPoints(void) { return allData; };
+        const std::vector<std::vector<std::shared_ptr<DimSpace>>>& GetBestInliers(void) { return bestInliers; };
+        std::shared_ptr<T> GetBestModel(void){ return bestModel; };
